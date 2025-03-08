4755b9929c741ec7e8fcf1ed55ed0ddad1539dcb
fabiomeira <fabio.meira@fabester.com.br>
2025-03-08 02:06:53 -0300

    updted

--- src/database/connection.js
+++ src/database/connection.js
@@ -4,11 +4,11 @@ const database = process.env.DATABASE;
 const username = process.env.USERNAME;
 const password = process.env.PASSWORD;
 const host = process.env.HOST; 
-const dialect = process.env.DIALECT;
+// const dialect = process.env.DIALECT;
 
 const connection = new Sequelize(database, username, password, {
     host, 
-    dialect,
+    dialect: "mysql",
     timezone: 'America/Sao_Paulo'
 })
 
