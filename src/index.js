e5863cd671a53818d9392c3b3e9d5666c9701191
fabiomeira <fabio.meira@fabester.com.br>
2025-03-08 01:51:00 -0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,28 +1,20 @@
-const express = require ('express')
-const app = express()
-require('dotenv').config()
+const express = require('express');
+const app = express();
+require('dotenv').config();
 const cors = require('cors');
-const sequelize = require('./database/connection'); 
-// const basicAuth = require('express-basic-auth')
+const sequelize = require('./database/connection');
+const routes = require('./routes');
 
-app.use(express.json())
-
-const protocol = process.env.PROTOCOL 
-const ip = require('ip').address()
-const port = process.env.PORT 
-
-// Permitir requisições de qualquer origem
+app.use(express.json());
 app.use(cors());
 
+// Sincronizando banco de dados
 sequelize.sync()
     .then(() => console.log('Banco de dados sincronizado'))
     .catch(err => console.error('Erro ao sincronizar o banco de dados:', err));
 
-const routes = require('./routes')
+// Usando as rotas
+app.use('/api', routes);
 
-// app.use('/oticas', routes); 
-
-app.use(routes)
-
-app.listen(port, () => console.log(`
-    Serviço rodando na porta ${port} ou ${protocol}:${ip}:${port}`));
+// Exportar para Vercel
+module.exports = app;
