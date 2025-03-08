4d300d7b62348e04dd2924b4844a7b8a0aba9451
fabiomeira <fabio.meira@fabester.com.br>
2025-03-08 03:15:09 -0300

    index updated

--- src/index.js
+++ src/index.js
@@ -20,10 +20,10 @@ sequelize.sync()
 // Usando as rotas
 app.use('/oticas', routes);
 
-app.use(routes);
+// app.use(routes);
 
-app.listen(port, () => console.log(`
-    Serviço rodando na porta ${port} ou ${protocol}:${ip}:${port}`));
+// app.listen(port, () => console.log(`
+//     Serviço rodando na porta ${port} ou ${protocol}:${ip}:${port}`));
 
 // Exportar para Vercel
 module.exports = app;
