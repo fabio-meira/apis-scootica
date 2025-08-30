78a4f412bf2232c64e116dbc2d49130633cf6562
fabiomeira <fabio.meira@fabester.com.br>
2025-08-30 17:33:29 -0300

    connection updated

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,48 +1,3 @@
-// const Sequelize = require('sequelize');
-// const mysql2 = require('mysql2');
-
-// const database = process.env.DATABASE;
-// const username = process.env.USERNAME;
-// const password = process.env.PASSWORD;
-// const host = process.env.HOST; 
-// // const dialect = process.env.DIALECT;
-
-// const connection = new Sequelize(database, username, password, {
-//     host, 
-//     dialect: "mysql",
-//     dialectModule: require('mysql2'),
-//     timezone: 'America/Sao_Paulo'
-// })
-
-// module.exports = connection
-
-// Abaixo configuração para a publicação no vercel
-
-// const { Sequelize } = require('sequelize');
-// const mysql2 = require('mysql2');
-
-// const database = process.env.DATABASE;
-// const username = process.env.USERNAME;
-// const password = process.env.PASSWORD;
-// const host = process.env.HOST;
-
-// const connection = new Sequelize(database, username, password, {
-//     host,
-//     dialect: "mysql",
-//     dialectModule: require('mysql2'),
-//     timezone: "-03:00", // Usando UTC-3 para São Paulo
-//     logging: false,  // Desativa logs do Sequelize para melhorar performance no Vercel
-//     pool: {           // Otimiza conexões para ambiente Serverless
-//         max: 5,
-//         min: 0,
-//         idle: 10000,
-//         acquire: 30000
-//     }
-// });
-
-// module.exports = connection;
-
-
 const { Sequelize } = require('sequelize');
 const mysql2 = require('mysql2');
 
@@ -71,6 +26,10 @@ const connection = new Sequelize(database, username, password, {
     define: {
         freezeTableName: true, // Evita Sequelize modificar nomes das tabelas
         timestamps: true // Se o banco não usa createdAt/updatedAt, desative
+    },
+    dialectOptions: {
+        allowPublicKeyRetrieval: true, // Necessário para MySQL 8+ remoto
+        ssl: false                     // Desativa SSL se não estiver configurado
     }
 });
 
